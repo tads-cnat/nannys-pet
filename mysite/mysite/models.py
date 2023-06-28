fbf1c5c558244c41325d3cb3337a2ed70935cb33
Sergio Henrique <45646187+sergioh665@users.noreply.github.com>
2023-06-28 09:47:45 -0300

    Update models.py

--- mysite/mysite/models.py
+++ mysite/mysite/models.py
@@ -7,8 +7,8 @@ class Administrador(models.Model):
 
 class SolicitacaoCuidador(models.Model):
     comprovante = models.ImageField
-    fotoResidencia = models.ImageField
-    statusSolicitacao = models.CharField
+    foto_residencia = models.ImageField
+    status_solicitacao = models.CharField
     endereco = models.CharField(max_length=100)
     codigo = models.CharField
 
@@ -16,12 +16,12 @@ class Usuario(models.Model):
     nome = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     cpf = models.CharField(max_length=11)
-    telefone = models.IntegerField(max_length=11)
+    telefone = models.CharField(max_length=11)
     endereco = models.CharField(max_length=100)
 
 class Hospedagem(models.Model):
-    dataInicio = models.CharField
-    dataFinal = models.CharField
+    data_inicio = models.DateTimeField()
+    data_final = models.DateTimeField()
     status = models.IntegerField(max_length=100)
     pets = models.IntegerField(max_length=100)
     cuidador = models.CharField(max_length=100)
@@ -30,10 +30,10 @@ class Pet(models.Model):
     nome = models.CharField(max_length=100)
     idade = models.IntegerField(max_length=100)
     porte = models.CharField(max_length=100)
-    docil = models.BooleanField
-    codigo = models.CharField(max_length=100)
+    caracteristica = models.CharField(max_length=300)
 
 class Cuidador(models.Model):
-    diasDisponiveis = models.CharField
-    valorDiaria = models.IntegerField
+    dias_disponiveis = models.CharField
+    valor_diaria = models.IntegerField
     preferencia = models.CharField(max_length=300)
+    
