67e93eb1b84ae3d6094040e45c440403c915e3b0
Walber <walber.silva@navi.ifrn.edu.br>
2023-07-16 10:46:05 -0300

    Ajeitand os campos do form

--- mysite/nannypets/forms.py
+++ mysite/nannypets/forms.py
@@ -7,7 +7,9 @@ class SolicitacaoHospedagemForm(forms.ModelForm):
         fields = ['dia_inicial', 'dia_final', 'cuidador', 'pet']
         widgets = {
             'dia_inicial': forms.DateInput(attrs={'class': 'data-input'}),
+            'dia_inicial': forms.DateInput(attrs={'type': 'date'}),
             'dia_final': forms.DateInput(attrs={'class': 'data-input'}),
+            'dia_final': forms.DateInput(attrs={'type': 'date'}),
             'cuidador': forms.Select(attrs={'class': 'cuidador-select'}),
             'pet': forms.Select(attrs={'class': 'pet-select'}),
         }
