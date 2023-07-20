60ff055bc1d83ec6cc68f4315fea85231d1bd8a6
Walber <walber.silva@navi.ifrn.edu.br>
2023-07-20 09:42:05 -0300

    add oq faltava dnv

--- mysite/nannypets/urls.py
+++ mysite/nannypets/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', login_required(views.IndexView.as_view()), name = 'index'),
     path('cadastro/', views.cadastro, name = 'cadastro'),
     path('pets/',login_required(views.PetsView.as_view()), name = 'pets'),
+    path('pets/<int:pk>/',login_required(views.PetView.as_view()), name = 'pet'),
     path('minhas_solicitacoes/', login_required(views.SolicitacaoHospedagemView.as_view()), name = 'listar_solicitacoes'),
     path('hospedagem/', login_required(views.HospedagemView.as_view()), name = 'hospedagem'),
 ]
