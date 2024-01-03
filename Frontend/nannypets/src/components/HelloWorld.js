178e984478953a3a97718393c16313d3de445dc8
renatobernardinodasilva <115257340+renatobernardinodasilva@users.noreply.github.com>
2024-01-03 10:00:54 -0300

    Delete Frontend/nannypets directory

--- Frontend/nannypets/src/components/HelloWorld.js
+++
@@ -1,9 +0,0 @@
-function HelloWorld(){
-    return(
-        <footer>
-            <a href=""><button class="mostrar">Mostrar Resultados</button></a>
-        </footer>
-    )
-}
-
-export default HelloWorld
