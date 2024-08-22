c3841bf7c89a9affdc074f7c8acffbd8a37ecabe
Virginia Menezes <90802727+VCLM@users.noreply.github.com>
2024-08-22 13:15:52 +0000

    atualizando front

--- Frontend/curso-angular/src/app/RespostaCep.ts
+++
@@ -1,3 +0,0 @@
-export interface RespostaCep{
-    cidade: string;
-}
--- Frontend/curso-angular/src/app/Tutor.ts
+++
@@ -1,7 +0,0 @@
-export interface Tutor{
-    nome: string;
-    sobrenome: string;
-    cpf: string;
-    email: string;
-    data_nascimento: Date;
-}
